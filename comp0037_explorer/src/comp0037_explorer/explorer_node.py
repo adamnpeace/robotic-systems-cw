cf2db137ff26c621c5d380ee9bcaf2f3f6e3070d
adamnpeace <adam.n.peace@gmail.com>
2020-03-08 21:29:14 +0000

    Adds options for frontier based search

--- comp0037_explorer/src/comp0037_explorer/explorer_node.py
+++ comp0037_explorer/src/comp0037_explorer/explorer_node.py
@@ -1,4 +1,5 @@
 import rospy
+import math
 from geometry_msgs.msg  import Pose2D
 from nav_msgs.msg import Odometry
 
@@ -9,7 +10,7 @@ from explorer_node_base import ExplorerNodeBase
 
 class ExplorerNode(ExplorerNodeBase):
 
-    def __init__(self):
+    def __init__(self, detection_algorithm="fromRobot", heuristic="euclidean"):
         ExplorerNodeBase.__init__(self)
 
         self.blackList = []
@@ -19,6 +20,9 @@ class ExplorerNode(ExplorerNodeBase):
 
         # Set the pose to an initial value to stop things crashing
         self.currentCoords = (0, 0)
+
+        self.detection_algorithm = detection_algorithm
+        self.heuristic = heuristic
     
     # Get the pose of the robot. Store this in a Pose2D structure because
     # this is easy to use. Use radians for angles because these are used
@@ -36,16 +40,10 @@ class ExplorerNode(ExplorerNodeBase):
     def updateFrontiers(self):
         pass
 
-    def chooseNewDestination(self):
-
-
-#         print 'blackList:'
-#         for coords in self.blackList:
-#             print str(coords)
-
+    def chooseNewDestinationFromOrigin(self):
         candidateGood = False
         destination = None
-        smallestD2 = float('inf')
+        smallestDist = float('inf')
 
         for x in range(0, self.occupancyGrid.getWidthInCells()):
             for y in range(0, self.occupancyGrid.getHeightInCells()):
@@ -58,15 +56,53 @@ class ExplorerNode(ExplorerNodeBase):
                             break
                     
                     if candidateGood is True:
-                        d2 = (candidate[0] - self.currentCoords[0])**2+(candidate[1] - self.currentCoords[0])**2
+                        if self.heuristic == "euclidean":
+                            dist = math.sqrt(candidate[0]**2+(candidate[1]-0.5*self.occupancyGrid.getHeightInCells())**2)
+                        elif self.heuristic == "manhattan":
+                            dist = abs(candidate[0]) + abs(candidate[1]-0.5*self.occupancyGrid.getHeightInCells())
 
-                        if (d2 < smallestD2):
+                        if (dist < smallestDist):
                             destination = candidate
-                            smallestD2 = d2
-
+                            smallestDist = dist
         # If we got a good candidate, use it
-
         return candidateGood, destination
+
+    def chooseNewDestinationFromRobot(self):
+        candidateGood = False
+        destination = None
+        smallestDist = float('inf')
+
+        for x in range(0, self.occupancyGrid.getWidthInCells()):
+            for y in range(0, self.occupancyGrid.getHeightInCells()):
+                candidate = (x, y)
+                if self.isFrontierCell(x, y) is True:
+                    candidateGood = True
+                    for k in range(0, len(self.blackList)):
+                        if self.blackList[k] == candidate:
+                            candidateGood = False
+                            break
+                    
+                    if candidateGood is True:
+                        if self.heuristic == "euclidean":
+                            dist = math.sqrt((candidate[0] - self.currentCoords[0])**2+(candidate[1] - self.currentCoords[0])**2)
+                        elif self.heuristic == "manhattan":
+                            dist = abs(candidate[0] - self.currentCoords[0]) + abs(candidate[1] - self.currentCoords[0])
+
+                        if (dist < smallestDist):
+                            destination = candidate
+                            smallestDist = dist
+        # If we got a good candidate, use it
+        return candidateGood, destination
+
+    def chooseNewDestination(self):
+#         print 'blackList:'
+#         for coords in self.blackList:
+#             print str(coords)
+
+        if self.detection_algorithm == "fromRobot":
+            return self.chooseNewDestinationFromRobot()
+        elif self.detection_algorithm == "fromOrigin":
+            return self.chooseNewDestinationFromOrigin()
 
     def destinationReached(self, goal, goalReached):
         if goalReached is False:
